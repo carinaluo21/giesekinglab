6c47ddac00ae439339952c511485c25c375fea7e
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-10 17:39:52 -0400

    Update plot_isom_energy.py

--- plot_isom_energy.py
+++ plot_isom_energy.py
@@ -1,21 +1,38 @@
 import matplotlib.pyplot as plt
 
 data_file = input("What is the file containing the relative isomeric energy called?") 
-atom_num = []
-isom_energy = []
+pos_atom_num = []
+pos_isom_energy = []
+neutral_atom_num = []
+neutral_isom_energy = []
+neg_atom_num = []
+neg_isom_energy = []
 
 with open(data_file, 'r') as file:
     for line in file:
         line = line.strip().split()
-        atom_num.append(float(line[0]))
-        isom_energy.append(float(line[1]))
+        atom_num = line[0]
+        charge = line[1]
+        isom_energy = line[2]
+        if charge == "1":
+            pos_atom_num.append(atom_num)
+            pos_isom_energy.append(isom_energy)
+        elif charge == "0":
+            neutral_atom_num.append(atom_num)
+            neutral_isom_energy.append(isom_energy)
+        elif charge == "-":
+            neg_atom_num.append(atom_num)
+            neg_isom_energy.append(isom_energy)
+        else:
+            echo "error"
 
 # Create scatter plot
-plt.scatter(atom_num, isom_energy)
-
-# Set plot labels
+plt.scatter(pos_atom_num, pos_isom_energy, color="red", label="cationic")
+plt.scatter(neg_atom_num, neg_isom_energy, color="blue", label="anionic")
+plt.scatter(neutral_atom_num, neutral_isom_energy, color="green", label="neutral")
 plt.xlabel('Atoms')
 plt.ylabel('Relative Isomeric Energy (eV)')
+plt.legend()
 
 # Save the plot
 output_file = input("What is the name of the file you would like to save the plot to?")
