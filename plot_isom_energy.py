2703e757ce0c74e214461f6a2d872a284877d811
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-10 14:19:43 -0400

    Update plot_isom_energy.py

--- plot_isom_energy.py
+++ plot_isom_energy.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
 
+data_file = input("What is the file containing the relative isomeric energy called?") 
+atom_num = []
+isom_energy = []
+
+with open(data_file, 'r') as file:
+    for line in file:
+        line = line.strip().split()
+        atom_num.append(float(line[0]))
+        isom_energy.append(float(line[1]))
+
+# Create scatter plot
+plt.scatter(atom_num, isom_energy)
+
+# Set plot labels
+plt.xlabel('Atoms')
+plt.ylabel('Relative Isomeric Energy (eV)')
+
+# Save the plot
+output_file = input("What is the name of the file you would like to save the plot to?")
+plt.savefig(output_file)
