77e89559614e82b86c6b3a81b77a43e2891668be
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-11 16:31:57 -0400

    Update comp_expense.sh

--- comp_expense.sh
+++ comp_expense.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# loop over all files in the current directory
-for file in *; do
+# Loop over all files in the current directory
+for file in ./; do
 	if [[ $file == *.log ]]; then
-		if grep -q "genecp" "$file"; then
-			basisset="augccpvdzpp"
-		else
-			basisset="def2-SVP"
-		fi
+		software="Gaussian"
+		
 		elapsed_time=$(grep -m 1 "Elapsed time" "$file" | cut -d' ' -f4-)
 		days=$(echo "$elapsed_time" | awk -F'[ .:]+' '{print $2}')
 		hours=$(echo "$elapsed_time" | awk -F'[ .:]+' '{print $4}')
 		minutes=$(echo "$elapsed_time" | awk -F'[ .:]+' '{print $6}')
  		seconds=$(echo "$elapsed_time" | awk -F'[ .:]+' '{print $8"."$9}')
     		total_minutes=$(echo "$days * 24 * 60 + $hours * 60 + $minutes + $seconds/60" | bc -l)
-    		nstates=$(grep -m 1 -o 'NStates=[^,]*' "$file" | cut -c 9-)
-    		method=$(grep -m 1 -o "# .*(" "$file" | cut -c 3- | rev | cut -c 2- | rev)
-    		echo "$file $method $basisset $nstates $total_minutes" >> computationalexpense
  	fi
 
-  	if [[ $file == *.out ]]; then
+  	elif [[ $file == *.out ]]; then
+		software="ORCA"
+		
     		total_run_time=$(grep -m 1 "TOTAL RUN TIME" "$file" | cut -d' ' -f4-)
     		days=$(echo "$total_run_time" | awk -F'[ .:]+' '{print $1}')
     		hours=$(echo "$total_run_time" | awk -F'[ .:]+' '{print $3}')
@@ -27,39 +23,60 @@ for file in *; do
     		seconds=$(echo "$total_run_time" | awk -F'[ .:]+' '{print $7}')
     		msec=$(echo "$total_run_time" | awk -F'[ .:]+' '{print $9}')
     		total_minutes=$(echo "$days * 24 * 60 + $hours * 60 + $minutes + $seconds/60 + $msec/60000" | bc -l)
-		if echo "$file" | grep -q "^EOM-CCSD_"; then
-    			method="EOM-CCSD"
-		elif echo "$file" | grep -q "^STEOM-CCSD"; then
-    			method="STEOM-CCSD"
-		elif echo "$file" | grep -q "^EOM-DLPNO-CCSD"; then
-                        method="EOM-DLPNO-CCSD"
-		elif echo "$file" | grep -q "^bt-PNO-EOM-CCSD"; then
-                        method="bt-PNO-EOM-CCSD"
-		elif echo "$file" | grep -q "^bt-PNO-STEOM-CCSD"; then
-                        method="bt-PNO-STEOM-CCSD"
-                elif echo "$file" | grep -q "^STEOM-DLPNO-CCSD"; then
-                        method="STEOM-DLPNO-CCSD"
-		fi
+  	else
+		echo "Not sure what software $file is"
+	fi
+	
+	#Figure out what the method is 
+	if echo "$file" | grep -q "^EOM-CCSD_"; then
+    		method="EOM-CCSD"
+	elif echo "$file" | grep -q "^STEOM-CCSD"; then
+    		method="STEOM-CCSD"
+	elif echo "$file" | grep -q "^EOM-DLPNO-CCSD"; then
+		method="EOM-DLPNO-CCSD"
+	elif echo "$file" | grep -q "^bt-PNO-EOM-CCSD"; then
+		method="bt-PNO-EOM-CCSD"
+	elif echo "$file" | grep -q "^bt-PNO-STEOM-CCSD"; then
+		method="bt-PNO-STEOM-CCSD"
+	elif echo "$file" | grep -q "^STEOM-DLPNO-CCSD"; then
+		method="STEOM-DLPNO-CCSD"
+	else
+		echo "Cannot figure out the method for $file"
+	fi
+	
+	#Figure out how many excited states
+	if echo "$file" | grep -q "5"; then
+		nstates="5"
+	elif echo "$file" | grep -q "6"; then
+		nstates="6"
+	elif echo "$file" | grep -q "7"; then
+		nstates="7"
+	elif echo "$file" | grep -q "8"; then
+		nstates="8"
+	elif echo "$file" | grep -q "9"; then
+		nstates="9"
+	elif echo "$file" | grep -q "10"; then
+		nstates="10"
+	else
+		echo "Cannot figure out how many states in $file"
+	fi
+	
+	#Figure out the basis set
+	if echo "$file" | grep -q "augccpvdzpp"; then
+		basisset="augccpvdzpp"
+	elif echo "$file" | grep -q "def2SVP"; then
+		basisset="def2SVP"
+	else 
+		echo "Cannot figure out the basis set for $file"
+	fi
+	
+	#Figure out if the calculation is relativistic
+	if echo "$file" | grep -q "rel"; then
+		rel="yes"
+	else
+		rel="no"
+	fi 
 		
-		if echo "$file" | grep -q "augccpvdzpp"; then
-			basisset="augccpvdzpp"
-		elif echo "$file" | grep -q "def2SVP"; then
-                        basisset="def2SVP"
-		fi
-
-		if echo "$file" | grep -q "5"; then
-                        nstates="5"
-		elif echo "$file" | grep -q "6"; then
-                        nstates="6"
-		elif echo "$file" | grep -q "7"; then
-                        nstates="7"
-		elif echo "$file" | grep -q "8"; then
-                        nstates="8"
-		elif echo "$file" | grep -q "9"; then
-                        nstates="9"
-		elif echo "$file" | grep -q "10"; then
-                        nstates="10"
-		fi
-		echo "$file $method $basisset $nstates $total_minutes" >> computationalexpense
-  	fi
+	echo "For $file the software is $software the method is $method the basis set is $basis_set the states are $nstates and the time is $total_minutes minutes"
+	#echo "$software $method $basis_set $nstates $rel $total_minutes" >> computationalexpense
 done
