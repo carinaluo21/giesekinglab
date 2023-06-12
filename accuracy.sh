a46127f83f067ec9264d34a0620cbe39b5bb030b
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-12 17:37:25 -0400

    Update accuracy.sh

--- accuracy.sh
+++ accuracy.sh
@@ -22,21 +22,7 @@ find . -type f \( -name "*.log" -o -name "*.out" \) | while read -r filepath; do
       			echo "$file method is not recognized"
     		fi
   	elif [[ $file == *.out ]]; then
-		if [[ $file == "STEOM-CCSD"* ]]; then
-			if [[ $file == *"10"* ]]; then
-				excitedstate_1=$(grep -m 5 "IROOT=  1" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_2=$(grep -m 5 "IROOT=  2" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_3=$(grep -m 5 "IROOT=  3" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_4=$(grep -m 5 "IROOT=  4" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_5=$(grep -m 5 "IROOT=  5" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_6=$(grep -m 5 "IROOT=  6" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_7=$(grep -m 5 "IROOT=  7" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_8=$(grep -m 5 "IROOT=  8" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_9=$(grep -m 5 "IROOT=  9" "$filepath" | tail -n 1 | awk '{print $5}')
-				excitedstate_10=$(grep -m 4 "IROOT= 10" "$filepath" | tail -n 1 | awk '{print $5}')
-				echo "$excitedstate_1 $excitedstate_2 $excitedstate_3 $excitedstate_4 $excitedstate_5 $excitedstate_6 $excitedstate_7 $excitedstate_8 $excitedstate_9 $excitedstate_10 in $file"
-			fi
-		elif [[ $file == "STEOM-DLPNO-CCSD"* ]]; then
+		if [[ $file == "STEOM-CCSD"* || $file == "STEOM-DLPNO-CCSD"* || $file == "EOM-DLPNO-CCSD"* ]]; then
 			if [[ $file == *"10"* ]]; then
 				excitedstate_1=$(grep -m 5 "IROOT=  1" "$filepath" | tail -n 1 | awk '{print $5}')
 				excitedstate_2=$(grep -m 5 "IROOT=  2" "$filepath" | tail -n 1 | awk '{print $5}')
