097471973fae15069af05f23a352e490be257a7d
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-12 16:51:41 -0400

    Update accuracy.sh

--- accuracy.sh
+++ accuracy.sh
@@ -3,29 +3,47 @@
 find . -type f \( -name "*.log" -o -name "*.out" \) | while read -r filepath; do
 	file="${filepath##*/}"
 	if [[ $file == *.log ]]; then
-	  if [[ $file == "EOM-CCSD"* ]]; then
-      if [[ $file == *"10"* ]]; then
-		    excitedstate_1=$(grep -m 2 "Excited State   1" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_2=$(grep -m 2 "Excited State   2" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_3=$(grep -m 2 "Excited State   3" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_4=$(grep -m 2 "Excited State   4" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_5=$(grep -m 2 "Excited State   5" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_6=$(grep -m 2 "Excited State   6" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_7=$(grep -m 2 "Excited State   7" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_8=$(grep -m 2 "Excited State   8" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_9=$(grep -m 2 "Excited State   9" "$filepath" | tail -n 1 | awk '{print $5}')
-        excitedstate_10=$(grep -m 2 "Excited State  10" "$filepath" | tail -n 1 | awk '{print $5}')
-        echo "$excitedstate_1 $excitedstate_2 $excitedstate_3 $excitedstate_4 $excitedstate_5 $excitedstate_6 $excitedstate_7 $excitedstate_8 $excitedstate_9 $excitedstate_10 in $file"
-      else
-        echo "There are not 10 excited states"
-      fi
-    else 
-      echo "Not an EOM-CCSD file"
-    fi
-  else
-    echo "Not a log file"
-  fi
+		if [[ $file == "EOM-CCSD"* ]]; then
+	  		if [[ $file == *"10"* ]]; then
+				excitedstate_1=$(grep -m 2 "Excited State   1" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_2=$(grep -m 2 "Excited State   2" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_3=$(grep -m 2 "Excited State   3" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_4=$(grep -m 2 "Excited State   4" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_5=$(grep -m 2 "Excited State   5" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_6=$(grep -m 2 "Excited State   6" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_7=$(grep -m 2 "Excited State   7" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_8=$(grep -m 2 "Excited State   8" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_9=$(grep -m 2 "Excited State   9" "$filepath" | tail -n 1 | awk '{print $5}')
+        			excitedstate_10=$(grep -m 2 "Excited State  10" "$filepath" | tail -n 1 | awk '{print $5}')
+        			echo "$excitedstate_1 $excitedstate_2 $excitedstate_3 $excitedstate_4 $excitedstate_5 $excitedstate_6 $excitedstate_7 $excitedstate_8 $excitedstate_9 $excitedstate_10 in $file"
+        		else
+        			echo "$file does not have 10 excited states"
+      			fi
+		elif [[ $file == "EOM-CCSD"* ]]; then
+			if [[ $file == *"10"* ]]; then
+				excitedstate_1=$()
+				excitedstate_2=$()
+				excitedstate_3=$()
+				excitedstate_4=$()
+				excitedstate_5=$()
+				excitedstate_6=$()
+				excitedstate_7=$()
+				excitedstate_8=$()
+				excitedstate_9=$()
+				excitedstate_10=$()
+			else 
+				echo "$file does not have 10 excited states"
+			fi
+		
+		else 
+      			echo "$file is not an EOM-CCSD file"
+    		fi
+  	else
+    		echo "$file is not a log file"
+  	fi
 done
+
+
   
   	elif [[ $file == *.out ]]; then
 		software="ORCA"
