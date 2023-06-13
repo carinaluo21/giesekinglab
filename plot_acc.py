59f414345071b73f4fa5cd98aa668cca3048a347
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-13 14:57:08 -0400

    Update plot_acc.py

--- plot_acc.py
+++ plot_acc.py
@@ -20,7 +20,7 @@ label_dict = {label: i + 1 for i, label in enumerate(labels)}
 # Generate categorical scatter plot
 for number, label in data:
     x_value = label_dict[label]
-    plt.scatter(x_value, number, marker="s")
+    plt.scatter(x_value, number, marker="s", s=(10, 20))
 
 # Set x-axis tick labels
 plt.xticks(list(label_dict.values()), list(label_dict.keys()), rotation=90)
