c6d91c9b4152faec2ca19cfeb3a8ad678322818b
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-12 20:25:31 -0400

    Update plot_acc.py

--- plot_acc.py
+++ plot_acc.py
@@ -12,17 +12,16 @@ with open(input_file, 'r') as file:
         for number in numbers:
             data.append((number, label))
 
-print(data)
-#x = list(range(len(data)))
-#y = [d[0] for d in data]
-#labels = [d[1] for d in data]
+x = list(range(len(data)))
+y = [d[0] for d in data]
+labels = [d[1] for d in data]
 
-#plt.scatter(x, y)
+plt.scatter(x, y)
 
-#plt.xticks(x, labels, rotation='vertical')
+plt.xticks(x, labels, rotation='vertical')
 
-#plt.ylabel('Energy (eV)')
+plt.ylabel('Energy (eV)')
 
-#output_file=input("What do you want the plot to be called?")
-#plt.savefig(output_file)
+output_file=input("What do you want the plot to be called?")
+plt.savefig(output_file)
 
