a7fdb5a450550feaaca062ef83f6854c5c694bc2
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-13 22:23:43 -0400

    Update acc.sh

--- acc.sh
+++ acc.sh
@@ -37,7 +37,41 @@ find . -type f \( -name "*.log" -o -name "*.out" \) | while read -r filepath; do
 				excitedstate_8=$(grep -m 5 "IROOT=  8" "$filepath" | tail -n 1 | awk '{print $5}')
 				excitedstate_9=$(grep -m 5 "IROOT=  9" "$filepath" | tail -n 1 | awk '{print $5}')
 				excitedstate_10=$(grep -m 4 "IROOT= 10" "$filepath" | tail -n 1 | awk '{print $5}')
-				echo "$excitedstate_1 $excitedstate_2 $excitedstate_3 $excitedstate_4 $excitedstate_5 $excitedstate_6 $excitedstate_7 $excitedstate_8 $excitedstate_9 $excitedstate_10 $file" >> $output_file
+    				if [[ $file == "STEOM-CCSD"* ]]; then
+					method="STEOM-CCSD"
+     				elif [[ $file == "STEOM-DLPNO-CCSD"* ]]; then
+	 				method="STEOM-DLPNO-CCSD"
+      				elif [[ $file == "EOM-DLPNO-CCSD"* ]]; then
+	  				method="EOM-DLPNO-CCSD"
+       				elif [[ $file == "bt-PNO-EOM-CCSD"* ]]; then
+	   				method="bt-PNO-EOM-CCSD"
+				elif [[ $file == "bt-PNO-STEOM-CCSD"* ]]; then
+	   				method="bt-PNO-STEOM-CCSD"
+				elif [[ $file == "EOM-CCSD"* ]]; then
+	   				method="EOM-CCSD"
+				else
+    					echo "Not sure what the method is for $file"
+				fi
+
+				if [[ $file == *"def2TZVPPD"* ]]; then
+    					basis_set="def2TZVPPD"
+	 			elif [[ $file == *"augccpvdzpp"* ]]; then
+    					basis_set="augccpvdzpp"
+	 			elif [[ $file == *"def2SV_P"* ]]; then
+    					basis_set="def2SV_P"
+	 			elif [[ $file == *"def2SVP_"* ]]; then
+    					basis_set="def2SVP"
+	 			elif [[ $file == *"def2SVPD"* ]]; then
+    					basis_set="def2SVPD"
+	 			elif [[ $file == *"def2TZVP_"* ]]; then
+    					basis_set="def2TZVP"
+	 			elif [[ $file == *"def2TZVPD"* ]]; then
+    					basis_set="def2TZVPD"
+	 			elif [[ $file == *"def2TZVPP"* ]]; then
+    					basis_set="def2TZVPP"
+	 			else 
+     					echo "Can't figure out basis set for $file"
+				echo "$excitedstate_1 $excitedstate_2 $excitedstate_3 $excitedstate_4 $excitedstate_5 $excitedstate_6 $excitedstate_7 $excitedstate_8 $excitedstate_9 $excitedstate_10 ${method}/${basis_set}" >> $output_file
 			fi
 		else
 			echo "$file method is not recognized"
