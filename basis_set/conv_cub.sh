932b34fc8f7739f0502a1e18395293d9e2b77294
carinaluo21 <107739074+carinaluo21@users.noreply.github.com>
2023-06-14 16:58:33 -0400

    Update conv_cub.sh

--- basis_set/conv_cub.sh
+++ basis_set/conv_cub.sh
@@ -6,5 +6,9 @@ read file
 echo what orbital number?
 read orbital
 
+#Convert the .chk file to a .fchk file
 formchk $file.chk $file.fchk
+
+#Utilize the cubegen utility of Gaussian
+#Documentation can be found at https://gaussian.com/cubegen/
 cubegen 0 MO=$orbital $file.fchk ${file}_$orbital.cub 200 
